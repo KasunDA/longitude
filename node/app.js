0c31986b50094d088cf75f8b384628c5df977ee0
Mantas Norvaiša <mntnorv@gmail.com>
2014-02-23 21:11:32 +0200

    Added test SockJS server

--- node/app.js
+++ node/app.js
@@ -1,5 +1,44 @@
-var	path = require('path'),
-	db   = require('./models');
+var	path   = require('path'),
+	db     = require('./models'),
+	http   = require('http'),
+	sockjs = require('sockjs');
+
+var server  = http.createServer(),
+	sockets = sockjs.createServer(),
+	clients = {};
+
+function broadcast(message, exclude) {
+	for (var client in clients) {
+		if (clients.hasOwnProperty(client)) {
+			if (client != exclude) {
+				clients[client].write(JSON.stringify(message));
+			}
+		}
+	}
+}
+
+function onConnection(conn) {
+	clients[conn.id] = conn;
+
+	conn.on('data', function (data) {
+		data = JSON.parse(data);
+
+		if (data.type == 'auth') {
+			db.ApiToken.find({
+				where: {token: data.token},
+				include: [db.User]
+			}).success(function (token) {
+				conn.data.user = token.user;
+				console.log(user.values);
+				console.log('Successfully authenticated: ' + token.user.username);
+			});
+		}
+	});
+
+	conn.on('close', function() {
+		delete clients[conn.id];
+	});
+}
 
 db
 	.sequelize
@@ -8,9 +47,8 @@ db
 		if (!!err) {
 			console.log('Unable to connect to the database:', err);
 		} else {
-			console.log('Connection has been established successfully.');
-			db.User.find({id: 1}).complete(function (err, user) {
-				console.log(user.values);
-			});
+			sockets.on('connection', onConnection);
+			sockets.installHandlers(server);
+			server.listen(9999, '0.0.0.0');
 		}
 	});
